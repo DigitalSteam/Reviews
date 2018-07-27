cf834256a0aff2f83af4dca2018578f5e5bbbf6b
dyliman <dyyliman@gmail.com>
2018-07-27 12:02:54 -0700

    CSS Added

--- client/reviews.jsx
+++ client/reviews.jsx
@@ -15,14 +15,17 @@ class Reviews extends React.Component {
     const date = object.reviewDatePosted;
     return (
       <div className="reviewContainer">
-        <div className="date">{date}</div>
+        <div className={`recommended${recommended}`}></div>
+        <div className="date">{`POSTED: ${date}`}</div>
         <div className="review">{review}</div>
-        <div className="recommended">{recommended}</div>
         <div className="helpful">
-          <div className="yes">{helpful.yes}</div>
-          <div className="no">{helpful.no}</div>
-          <div className="funny">{helpful.funny}</div>
-          <div className="report">{helpful.report}</div>
+          <div>Was this review helpful?
+            <button className="buttonYes">&#9745; Yes</button>
+            <button className="buttonNo">&#9746; No</button>
+            <button className="buttonFunny">&#9786; Funny</button>
+          </div>
+          <div className="yes">{`${helpful.yes} people found this review helpful`}</div>
+          <div className="funny">{`${helpful.funny} people found this review funny`}</div>
         </div>
       </div>
     );
